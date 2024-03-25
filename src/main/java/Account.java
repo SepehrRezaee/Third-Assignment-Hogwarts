5c519c13a4e9998e4752a23c1233c2096e3ea254
Sepehr Rezaee <78574247+SepehrRezaee@users.noreply.github.com>
2024-03-25 11:38:05 +0330

    Update Account.java

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,24 +1,52 @@
-import java.util.UUID;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Account {
+
     private String username;
-    // TODO: Passwords should hashed
-    private String password;
+    private byte[] hashedPassword;
     private UUID accountID;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.accountID = UUID.randomUUID();
+        this.hashPassword(password);
+    }
+
+    private void hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes());
+            this.hashedPassword = md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public boolean validatePassword(String enteredPassword) {
-        //TODO
-        return false;
+        byte[] enteredHashedPassword = hashString(enteredPassword);
+        return MessageDigest.isEqual(this.hashedPassword, enteredHashedPassword);
     }
 
     @Override
     public void changeUsername(String newUsername) {
-        //TODO
+        this.username = newUsername;
     }
 
     @Override
     public void changePassword(String newPassword) {
-        //TODO
+        this.hashPassword(newPassword);
+    }
+
+    private byte[] hashString(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(input.getBytes());
+            return md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
